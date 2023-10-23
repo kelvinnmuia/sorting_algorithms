1409b01bf1198b1a3495ad952488c567a0ff935e
kelvinnmuia <kelvinnmuia@gmail.com>
2023-10-23 04:12:28 -0700

    created the bubble sort algorithm

---
+++ sort.h
@@ -0,0 +1,17 @@
+#ifndef SORT_H
+#define SORT_H
+
+/*--LIBRARIES--*/
+#include <stdlib.h>
+#include <stdio.h>
+/*--FUNCTION PROTOTYPES--*/
+/*--function to bubble sort integer array--*/
+void bubble_sort(int *array, size_t size);
+
+/*--function for printing the integer array--*/
+void print_array(const int *array, size_t size);
+
+/*--function for printing the list of integers--*/
+/*void print_list(const listint_t *list);*/
+
+#endif
